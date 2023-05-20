729c86cf2146c330ab46c6dc13cd0c1c07888c9e
Aditta Chakraborty <adittacse@gmail.com>
2023-05-20 12:55:06 +0600

    slider responsive updated

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -8,15 +8,15 @@ const Banner = () => {
         <div className="mb-10">
             <Carousel showThumbs={false} infiniteLoop autoPlay interval={5000} transitionTime={500} emulateTouch swipeable showArrows>
                 <div>
-                    <img  src="https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="Slide 1" />
+                    <img className="h-60 md:h-fit lg:h-[550px]" src="https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="Slide 1" />
                     <p className="legend">Marvel, Avengers & Star Wars Games are Available in Our Shop</p>
                 </div>
                 <div>
-                    <img src="https://images.unsplash.com/photo-1640499900704-b00dd6a1103a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80" alt="Slide 2" />
+                    <img className="h-60 md:h-fit lg:h-[550px]" src="https://images.unsplash.com/photo-1640499900704-b00dd6a1103a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80" alt="Slide 2" />
                     <p className="legend">Experience Luxury at its Best</p>
                 </div>
                 <div>
-                    <img  src="https://images.unsplash.com/photo-1614419349260-ad8317a964e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80" alt="Slide 3" />
+                    <img className="h-60 md:h-fit lg:h-[550px]" src="https://images.unsplash.com/photo-1614419349260-ad8317a964e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80" alt="Slide 3" />
                     <p className="legend">Unleash Your Adventure Spirit</p>
                 </div>
             </Carousel>
