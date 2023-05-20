5d4e0c1b0dea8e12f184d0c8d830af9702a3f338
Aditta Chakraborty <adittacse@gmail.com>
2023-05-20 22:14:59 +0600

    home page gallary animation updated

--- src/components/Home/Gallery/Gallery.jsx
+++ src/components/Home/Gallery/Gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = () => {
     
     return (
         <section className="mb-16">
-            <div data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="2000" className="container mx-auto px-4">
+            <div data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1500" className="container mx-auto px-4">
                 <h2 className="text-4xl font-bold text-gray-800 mb-6 text-center">Gallery</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     <div className="bg-white rounded-lg shadow-lg">
